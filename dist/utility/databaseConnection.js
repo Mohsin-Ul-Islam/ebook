bca86b8200ad6b6c71f2a16f9614ed1d33ed8358
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2020-07-21 18:32:28 +0500

    heroku update

--- dist/utility/databaseConnection.js
+++ dist/utility/databaseConnection.js
@@ -3,13 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var pg_1 = require("pg");
 var clientInstance = new pg_1.Client({
     connectionString: process.env.DB_URL,
-    ssl: process.env.NODE_ENV === 'development' ? { rejectUnauthorized: false } : true
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 clientInstance.connect()
     .then(function () {
     console.log('Connected to database');
 })
     .catch(function (err) {
-    console.log("Error connecting to database: " + err.message);
+    console.log("Error connecting to database: " + err);
 });
 exports.default = clientInstance;

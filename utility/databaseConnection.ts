23fb33ef4e90082b1403721a1c4ac5a18f4a4e23
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2020-07-21 18:20:23 +0500

    heroku update

--- utility/databaseConnection.ts
+++ utility/databaseConnection.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 const clientInstance = new Client({
-    connectionString: 'postgres://fsctjldaibsffr:03e8fe08783f66c234106a3223b04c0c967831730f4b0fb9531fdcbf53219769@ec2-52-72-221-20.compute-1.amazonaws.com:5432/dfo2ckfo3togjp',
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }

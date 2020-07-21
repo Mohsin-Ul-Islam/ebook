5321818643d1ee2e2c8a3bb19c9bda1ffb0edad9
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2020-07-21 18:17:36 +0500

    heroku update

--- utility/databaseConnection.ts
+++ utility/databaseConnection.ts
@@ -1,11 +1,13 @@
 import { Client } from 'pg';
 
-const clientInstance: Client = new Client(
-    {
-        connectionString: process.env.DB_URL,
-        ssl: false
+const clientInstance = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
     }
-);
+});
+
+
 
 clientInstance.connect()
     .then(() => {
